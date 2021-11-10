e281bc98b16baeb6ba30f4201f4031e837e41cf7
Masaki Nakano <admin@namachan10777.dev>
2021-11-10 14:51:40 +0900

    soft: hello world

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DWKB
+
 Depression Working Keyboard.
+
+## Build firmware
+
+```sh
+cd soft
+cargo build --release --target=thumbv7em-none-eabihf
+```
--- soft/src/main.rs
+++ soft/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
+use stm32l4::stm32l412;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = stm32l412::Peripherals::take().unwrap();
+    let gpioa = &peripherals.GPIOA;
+    gpioa.odr.modify(|_, w| w.odr0().set_bit());
+    loop {}
 }
